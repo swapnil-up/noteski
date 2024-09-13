331257e7cee10ca360e36546b0c3883c3b448ebc
swapnil-up <swapnilupadhyaykharel@gmail.com>
2024-09-13 22:52:33 +0545

    First repo

---
+++ android/app/src/main/kotlin/com/example/noteski/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.noteski
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    ),
+  );
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Noteski'),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: Text('Register'),
+        ),
+      ),
+    );
+  }
+}
